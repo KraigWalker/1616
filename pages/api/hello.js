94123f25c80ee3d783d7122e55c0206e38f8fcd4
Kraig Walker <kraig_walker@me.com>
2023-01-20 10:43:53 +0000

    create initial grid layout

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: 'edge',
 };
 
 export default function handler(req, res) {
--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,54 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="nav-container">
-      <nav aria-labelledby='nav-label'>
+      <header>
+        <nav aria-labelledby='nav-label' className={styles.header}>
           <h2 id="nav-label" className={styles.vh}>Site</h2>
-        <div>
-          <Link href="/"><span className={styles.vh}>16:16 home page</span></Link>
-        </div>
-        <ul>
-          <li><a>About</a></li>
-          <li><a>Log</a></li>
-          <li><a>Upcoming</a></li>
-          <li><a>Subscribe</a></li>
-          {/** @todo add space for a serviceworker installed status indicator, and notification counter */}
-        </ul>
-        </nav>
-        </div>
+          <div className={styles.logo}>
+            <Link href="/" className={styles.logoText}>16:16<span className={styles.vh}> home page</span></Link>
+          </div>
+          <div className={styles.selector}>
+
+          </div>
+          <div className={styles.podcast}><div className={'text'}>RSS</div></div>
+          <div className={styles.info}><div>i</div></div>
+          <div className={styles.newsletter}><div>i</div></div>
+          </nav>
+        </header>
       <main className={styles.main}>
-        
+        <div className={styles.noon}></div>
+        <div className={styles.calendar}>
+          <div className={styles.day}>
+            <div className={styles.text}>M</div>
+            <div className={styles.number}>16</div>
+          </div>
+          <div className={styles.day}>
+          <div className={styles.text}>T</div>
+            <div className={styles.number}>17</div>
+          </div>
+          <div className={styles.day}>
+            <div className={styles.text}>W</div>
+            <div className={styles.number}>18</div>
+          </div>
+          <div className={styles.day}>
+            <div className={styles.text}>T</div>
+            <div className={styles.number}>19</div>
+          </div>
+          <div className={styles.day}>
+            <div className={styles.text}>F</div>
+            <div className={styles.number}>20</div>
+          </div>
+          <div className={styles.day}>
+            <div className={styles.text}>S</div>
+            <div className={styles.number}>21</div>
+          </div>
+          <div className={`${styles.day} ${styles.last}`}>
+            <div className={styles.text}>S</div>
+            <div className={styles.number}>22</div>
+          </div>
+        </div>
       </main>
+      <footer className={styles.footer}></footer>
     </>
   )
 }
